d81d3136669089efc0afa4d101e5cd4131069c5c
Biprodas Roy <biprodas.cse@gmail.com>
2024-01-10 01:04:45 +0600

    Solution: // 88. Merge Sorted Array

---
+++ LeetCode/88/solution.java
@@ -0,0 +1,9 @@
+// 88. Merge Sorted Array
+class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    for(int j=0, i=m; j<n; j++){
+      nums1[i++] = nums2[j];
+    }
+    Arrays.sort(nums1);
+  }
+}
