90af06ab4c503990ca25faa699262294fb873dd5
Biprodas Roy <biprodas.cse@gmail.com>
2024-01-21 16:32:38 +0600

    Solution: 198. House Robber

---
+++ LeetCode/198/solution.cpp
@@ -0,0 +1,20 @@
+// 198. House Robber
+class Solution {
+public:
+
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size() , -1);
+        return solve(0, nums, dp);
+    }
+
+    int solve(int i, vector<int>& nums, vector<int>& dp){
+        if(i>=nums.size()) return 0;
+
+        if(dp[i]!=-1) return dp[i];
+        
+        int pick = nums[i] + solve(i+2, nums, dp);
+        int no_pick = solve(i+1, nums, dp);
+
+        return dp[i] = max(pick, no_pick);
+    }
+};
