52b730036f822e59f59e8a58587630efeed415e5
Biprodas Roy <biprodas.cse@gmail.com>
2024-01-23 01:14:21 +0600

    oo

--- LeetCode/283/solution.cpp
+++ LeetCode/283/solution.cpp
@@ -3,16 +3,14 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int sz = nums.size();
-        for(int i=0, j=0; i<sz && j<sz; i++){
+        for(int i=0, j=0; i<sz; i++){
             if(nums[i]==0){
                 if(j<i) j=i;
                 while(nums[j]==0){
                     j++;
                     if(j==sz) return;
                 }
-                int tmp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = tmp;
+                swap(nums[i], nums[j]);
             }
         }
     }
--- LeetCode/283/solution.java
+++ LeetCode/283/solution.java
@@ -2,13 +2,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int sz = nums.length;
-        for(int i=0, j=0; i<sz && j<sz; i++){
+        for(int i=0, j=0; i<sz; i++){
             if(nums[i]==0){
                 if(j<i) j=i;
                 while(nums[j]==0){
                     j++;
-                    if(j==sz) 
-                        return;
+                    if(j==sz) return;
                 }
                 int tmp = nums[i];
                 nums[i] = nums[j];
