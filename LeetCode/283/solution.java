ba0c64350f0006becab2cfa14cb5b27573adb12c
Biprodas Roy <biprodas.cse@gmail.com>
2024-01-23 00:58:03 +0600

    Solution: 283. Move Zeroes

---
+++ LeetCode/283/solution.java
@@ -0,0 +1,19 @@
+// 283. Move Zeroes
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int sz = nums.length;
+        for(int i=0, j=0; i<sz && j<sz; i++){
+            if(nums[i]==0){
+                if(j<i) j=i;
+                while(nums[j]==0){
+                    j++;
+                    if(j==sz) 
+                        return;
+                }
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+        }
+    }
+}
