4e6fcaa4da38e9ce41bd299f335ff3dd0720ab30
Biprodas Roy <biprodas.cse@gmail.com>
2024-02-02 01:03:53 +0600

    Solution: 2966. Divide Array Into Arrays With Max Difference

---
+++ LeetCode/2966/solution.cpp
@@ -0,0 +1,16 @@
+// 2966. Divide Array Into Arrays With Max Difference
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        for(int i=0; i<nums.size(); i+=3){
+            if(nums[i+2]-nums[i] > k) 
+                return {};
+            ans.push_back({nums[i], nums[i+1], nums[i+2]});
+        }
+
+        return ans;
+    }
+};
