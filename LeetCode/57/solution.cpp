70df7f8e4c57e64d23e5fb6ba569b02fd6144f3d
Biprodas Roy <biprodas.cse@gmail.com>
2024-03-17 17:10:58 +0600

    Solution: 57. Insert Interval

---
+++ LeetCode/57/solution.cpp
@@ -0,0 +1,30 @@
+// 57. Insert Interval
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size();
+        vector<vector<int>> result;
+
+        int i = 0;
+
+        while(i<n && intervals[i][1] < newInterval[0]){
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+        while(i<n && newInterval[1] >= intervals[i][0]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        result.push_back(newInterval);
+
+        while(i<n){
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+        return result;
+    }
+};
