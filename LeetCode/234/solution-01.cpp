ff05299e7436eec1bd256117ea9cb157b08de691
Biprodas Roy <biprodas.cse@gmail.com>
2024-03-22 19:23:12 +0600

    Solution: 234. Palindrome Linked List

---
+++ LeetCode/234/solution-01.cpp
@@ -0,0 +1,29 @@
+// 234. Palindrome Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> list;
+        while(head){
+            list.push_back(head->val);
+            head = head->next;
+        }
+
+        int left=0, right=list.size()-1;
+        while(left < right && list[left]==list[right]){
+            left++;
+            right--;
+        }
+
+        return left >= right;
+    }
+};
---
+++ LeetCode/234/solution-02.cpp
@@ -0,0 +1,46 @@
+// 234. Palindrome Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* rev = reverseList(slow);
+
+        while(rev) {
+            if(head->val != rev->val) {
+                return false;
+            }
+            head = head->next;
+            rev = rev->next;
+        }
+
+        return true;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
