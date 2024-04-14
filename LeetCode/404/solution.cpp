94b5a8af5af67b017ff387557af894054258c2ad
Biprodas Roy <biprodas.cse@gmail.com>
2024-04-14 17:25:12 +0600

    Solution: 404. Sum of Left Leaves

---
+++ LeetCode/404/solution.cpp
@@ -0,0 +1,32 @@
+// 404. Sum of Left Leaves
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        return dfs(root, false);
+    }
+
+    int dfs(TreeNode* node, bool isLeft){
+        if(!node) return 0;
+        
+        if (!node->left && !node->right && isLeft) {
+            return node->val;
+        }
+
+        int leftSum = dfs(node->left, true);
+        int rightSum = dfs(node->right, false);
+
+        return leftSum + rightSum;
+    }
+};
